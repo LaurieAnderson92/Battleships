bde6db7a1e402403ddf7765d2bf09c2d2daf60cf
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-05-06 15:43:01 +0000

    build: created display_title funtion

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def display_title():
+    print('               |    |    |    ')             
+    print('              )_)  )_)  )_)   ')           
+    print('             )___))___))___)  ')          
+    print('            )____)____)_____) ')
+    print('          _____|____|____|______')
+    print(' ---------\\                   /---------')
+    print('         ^^^^^ ^^^^^^^^^^^^^^^^^^^^^')
+    print('        ^^^^      ^^^^     ^^^    ^^')
+    print('                 ^^^^      ^^^')
+    print('Welcome to Battleships! a nautical game of tactics.')
+
+display_title()
